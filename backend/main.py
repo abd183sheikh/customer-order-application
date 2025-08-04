d1722e7ead3aeb65ccd5ce57a495e139847c24f5
Abdullah Noor <abd183sheikh.gmail.com>
2025-08-04 14:44:01 +0530

    commit done

--- backend/main.py
+++ backend/main.py
@@ -20,4 +20,5 @@ def get_users():
     return jsonify(df.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
--- src/component/Fetchdata.jsx
+++ src/component/Fetchdata.jsx
@@ -4,7 +4,7 @@ const Fetchdata = () => {
 const [orders, setOrders] = useState([]);
 
  useEffect(() => {
-  fetch('http://localhost:5000/api/orders')
+  fetch('http://192.168.29.132:5000/api/orders')
     .then((res) => res.json())
     .then((data) => {
       console.log("Fetched data:", data);  // 👈 Add this
@@ -14,13 +14,39 @@ const [orders, setOrders] = useState([]);
 }, []);
   return (
     <div>
-      <h2>Orders List</h2>
-      <ul>
-        {orders.map((order, index) => (
-          <li key={index}>{JSON.stringify(order)}</li>
-        ))}
-      </ul>
-    </div>
+  <h2>Orders List</h2>
+  <table border="1" cellPadding="5">
+    <thead>
+      <tr>
+        <th>Order ID</th>
+        <th>User ID</th>
+        <th>Status</th>
+        <th>Gender</th>
+        <th>Created At</th>
+        <th>Returned At</th>
+        <th>Shipped At</th>
+        <th>Delivered At</th>
+        <th>Items</th>
+      </tr>
+    </thead>
+    <tbody>
+      {orders.map((order, index) => (
+        <tr key={index}>
+          <td>{order.order_id}</td>
+          <td>{order.user_id}</td>
+          <td>{order.status}</td>
+          <td>{order.gender}</td>
+          <td>{order.created_at}</td>
+          <td>{order.returned_at}</td>
+          <td>{order.shipped_at}</td>
+          <td>{order.delivered_at}</td>
+          <td>{order.num_of_item}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
   )
 }
 
